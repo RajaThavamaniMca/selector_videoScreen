7abab25408c5a9f10a45f75b4efb2de95f0e1089
Raja Thavamani <37396319+RajaThavamaniMca@users.noreply.github.com>
2019-01-24 12:27:21 +0530

    Add files via upload
    video screen

---
+++ src/App.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+import "./App.css"
+
+class App extends Component {
+  
+  render() { 
+    return ( 
+        <div className="container main_container">
+            <div className="row">
+                <div className="col-md-9 main_1st_column_div">
+                    <img src="../assets/images/bg.png" width="100%" className="big_image img-fluid" alt="Caption Pick Video" />
+                    <button className="btn btn-danger caption_main_button">PICK CAPTION</button>
+                    <h6 className="main_video_description"> Lorem Ipsum dolor set amit, adipscing elit emit solor noecn saisr consectetur elit.  Lorem Ipsum dolor set amit, adipscing elit emit solor noecn saisr consectetur elit.</h6>
+                </div>
+      
+                <div className="col-md-3 main_2nd_column_div">
+                    <div className="row margin-bottom">
+                        <div className="col-md-4 small_img_div">
+                            <img src="../assets/images/sm.png" className="img-fluid" alt="Caption Pick Video" />
+                        </div>
+                        <div className="col-md-8 align-self-center last_section_div">
+                            <div className="row">
+                                <div className="col-md-7 small_button_div">
+                                <button className="btn btn-primary">CAPTAIN PICK</button>
+                            </div>
+                            <div className="col-md-5 time_title_div"><h6 className="time_title">4 hours ago</h6></div>
+                        </div>
+                    <div className="row">
+                      <div className="col-md-10">
+                          <h6 className="small_video_description">Lorem ipsum dolor sit amet, consectetur adipscing elit. Non donec elitign cet.</h6>
+                      </div>
+                    </div>
+                </div>
+          </div>
+
+          <div className="row margin-bottom">
+               <div className="col-md-4 small_img_div">
+                   <img src="../assets/images/sm.png" className="img-fluid" alt="Caption Pick Video" />
+               </div>
+               <div className="col-md-8 align-self-center last_section_div">
+                   <div className="row">
+                       <div className="col-md-7 small_button_div">
+                           <button className="btn btn-primary">CAPTAIN PICK</button>
+                       </div>
+                       <div className="col-md-5 time_title_div"><h6 className="time_title">4 hours ago</h6></div>
+                   </div>
+                   <div className="row">
+                       <div className="col-md-10">
+                           <h6 className="small_video_description">Lorem ipsum dolor sit amet, consectetur adipscing elit. Non donec elitign cet.</h6>
+                       </div>
+                   </div>
+                   
+               </div>
+           </div>
+
+           <div className="row margin-bottom">
+                   <div className="col-md-4 small_img_div">
+                       <img src="../assets/images/sm.png" className="img-fluid" alt="Caption Pick Video" />
+                   </div>
+                   <div className="col-md-8 align-self-center last_section_div">
+                       <div className="row">
+                           <div className="col-md-7 small_button_div">
+                               <button className="btn btn-primary">CAPTAIN PICK</button>
+                           </div>
+                           <div className="col-md-5 time_title_div"><h6 className="time_title">4 hours ago</h6></div>
+                       </div>
+                       <div className="row">
+                           <div className="col-md-10">
+                               <h6 className="small_video_description">Lorem ipsum dolor sit amet, consectetur adipscing elit. Non donec elitign cet.</h6>
+                           </div>
+                       </div>
+                       
+                   </div>
+               </div>
+
+               <div className="row margin-bottom">
+                       <div className="col-md-4 small_img_div">
+                           <img src="../assets/images/sm.png" className="img-fluid" alt="Caption Pick Video" />
+                       </div>
+                       <div className="col-md-8 align-self-center last_section_div">
+                           <div className="row">
+                               <div className="col-md-7 small_button_div">
+                                   <button className="btn btn-primary">CAPTAIN PICK</button>
+                               </div>
+                               <div className="col-md-5 time_title_div"><h6 className="time_title">4 hours ago</h6></div>
+                           </div>
+                           <div className="row">
+                               <div className="col-md-10">
+                                   <h6 className="small_video_description">Lorem ipsum dolor sit amet, consectetur adipscing elit. Non donec elitign cet.</h6>
+                               </div>
+                           </div>
+                           
+                       </div>
+                   </div>
+
+                        <div className="row margin-bottom">
+                           <div className="col-md-4 small_img_div">
+                               <img src="../assets/images/sm.png" className="img-fluid" alt="Caption Pick Video" />
+                           </div>
+                           <div className="col-md-8 align-self-center last_section_div">
+                               <div className="row">
+                                   <div className="col-md-7 small_button_div">
+                                       <button className="btn btn-primary">CAPTAIN PICK</button>
+                                   </div>
+                                   <div className="col-md-5 time_title_div"><h6 className="time_title">4 hours ago</h6></div>
+                               </div>
+                               <div className="row">
+                                   <div className="col-md-10">
+                                       <h6 className="small_video_description">Lorem ipsum dolor sit amet, consectetur adipscing elit. Non donec elitign cet.</h6>
+                                   </div>
+                               </div>
+                               
+                           </div>
+                        </div>
+
+                        <div className="row margin-bottom">
+                           <div className="col-md-4 small_img_div">
+                               <img src="../assets/images/sm.png" className="img-fluid" alt="Caption Pick Video" />
+                           </div>
+                           <div className="col-md-8 align-self-center last_section_div">
+                               <div className="row">
+                                   <div className="col-md-7 small_button_div">
+                                       <button className="btn btn-primary">CAPTAIN PICK</button>
+                                   </div>
+                                   <div className="col-md-5 time_title_div"><h6 className="time_title">4 hours ago</h6></div>
+                               </div>
+                               <div className="row">
+                                   <div className="col-md-10">
+                                       <h6 className="small_video_description">Lorem ipsum dolor sit amet, consectetur adipscing elit. Non donec elitign cet.</h6>
+                                   </div>
+                               </div>
+                               
+                           </div>
+                        </div>
+
+
+                     
+      </div>
+  </div>
+</div>
+     );
+  }
+}
+ 
+export default App;
